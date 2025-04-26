8d8eb87f84dc25774b6b68e86018eee8115874cd
ThatGuySam <sam@sam.lc>
2025-04-26 09:10:58 -0500

    update: add verifications to servers schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,8 +1,22 @@
 import { z } from "zod";
 
+const verificationTags = [
+	"passes-mcp-shield",
+	"has-auth",
+	"official",
+	"openid-connect",
+	"oauth-2.0",
+	"oauth-2.1",
+] as const;
+
+/**
+ * @description The schema for the MCP Servers collection
+ */
 export const ServerSchema = z.object({
 	title: z.string(),
 	description: z.string(),
-	date: z.date(),
+	repoUrl: z.string().url(),
+	verifications: z.array(z.enum(verificationTags)),
+	lastUpdated: z.string(),
 	ogImage: z.string().startsWith("/"),
 });
